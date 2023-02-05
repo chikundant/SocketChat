7443d949700c151ff97d1dfcad8834d8c26c4e14
Dima Chikunda <chikunda.dima@gmail.com>
2023-02-05 16:43:01 +0200

    added registration

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import config
-
+import json
 
 class Client:
     def __init__(self):
@@ -9,26 +9,43 @@ class Client:
         self.PORT = config.PORT
         self.FORMAT = config.FORMAT
 
-        self.s = socket.socket()
-        self.s.connect((self.HOST, self.PORT))
+        self.client = socket.socket()
+        self.client.connect((self.HOST, self.PORT))
 
     def __receive(self):
         while True:
-            data = self.s.recv(1024)
+            data = self.client.recv(1024)
             print(data.decode(self.FORMAT))
 
     def __send(self):
         while True:
             msg = input()
-            self.s.send(msg.encode(self.FORMAT))
+            self.client.send(msg.encode(self.FORMAT))
+
+    def __log_in(self):
+        print("Log in")
+        username = input('username: ')
+        password = input('password: ')
+        self.client.send(json.dumps(['log', username, password]).encode(self.FORMAT))
+
+        return self.client.recv(1024).decode(self.FORMAT)
+
+    def __registration(self):
+        print("Registration")
+        username = input('username: ')
+        password = input('password: ')
+        self.client.send(json.dumps(['reg', username, password]).encode(self.FORMAT))
+        print("Success!")
 
     def start_client(self):
-        self.s.send(input('username: ').encode(self.FORMAT))
-
-        receive_thread = threading.Thread(target=self.__receive)
-        send_thread = threading.Thread(target=self.__send)
-        send_thread.start()
-        receive_thread.start()
+        if self.__log_in() == 'True':
+            receive_thread = threading.Thread(target=self.__receive)
+            send_thread = threading.Thread(target=self.__send)
+            send_thread.start()
+            receive_thread.start()
+        else:
+            self.__registration()
+            self.start_client()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -16,27 +16,43 @@ class ChatServer:
         self.server.listen()
 
         self.users = {}
+        self.user_data = []
 
     def __broadcast(self, data, sender):
         for user in self.users.keys():
             if user != sender:
-                user.send(f"{self.users[user]}: {data.decode(self.FORMAT)}".encode(self.FORMAT))
+                user.send(f"{self.users[sender]}: {data.decode(self.FORMAT)}".encode(self.FORMAT))
+
+    def __log_in(self, data, client):
+
+        if data[0] == 'reg':
+            with open('users.json', 'w') as f:
+                self.user_data.append({"username": data[1], "password": data[2]})
+                json.dump(self.user_data, f)
+            self.__write_user({client: data[1]})
+        if data[0] == 'log':
+            for user in self.user_data:
+                if user['username'] == data[1] and user['password'] == data[2]:
+                    self.__write_user({client: data[1]})
+                    client.send('True'.encode(self.FORMAT))
+                    return
+            client.send('False'.encode(self.FORMAT))
 
     def __receive(self, client):
         try:
-
             while True:
                 data = client.recv(1024)
-                if client not in self.users:
-                    self.__write_user({client: data.decode(self.FORMAT)})
-                    continue
-                if data:
-                    self.__broadcast(data, client)
+                try:
+                    self.__log_in(json.loads(data), client)
+                except:
+                    if data:
+                         self.__broadcast(data, client)
         except Exception as e:
             self.server.close()
             print("[SERVER CONNECTION CLOSED]")
 
     def start_server(self):
+        self.load()
         print('[SERVER IS WORKING]')
         print(f"[{datetime.datetime.now()}]")
         try:
@@ -52,6 +68,14 @@ class ChatServer:
 
     def __write_user(self, user: {}):
         self.users.update(user)
+
+    def load(self):
+        try:
+            with open('users.json', 'r') as f:
+                data = f.read()
+                self.user_data = json.loads(data)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
